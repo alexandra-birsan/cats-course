42ae12e5f3a2cf99492e9941c180b7882a630240
alexandra-birsan <birsan.dev@gmail.com>
2023-04-10 18:11:42 +0200

    Initial work

---
+++ src/main/scala/part1recap/Essentials.scala
@@ -0,0 +1,7 @@
+package part1recap
+
+object Essentials extends App {
+
+  // our programs consist of values & expressions, which are then combined to obtain new info
+
+}
---
+++ src/main/scala/part1recap/Implicits.scala
@@ -0,0 +1,58 @@
+package part1recap
+
+import part1recap.Implicits.Person
+
+object Implicits {
+
+  // implicit classes
+  final case class Person(name: String) {
+    def greet() = s"Hi, my name is $name!"
+  }
+
+  implicit class ImpersonableString(name: String) {
+    def greet() = Person(name).greet()
+  }
+
+  // this is the explicit way, but we want to attach the greet() method to a String (this is the purpose of implicit classes)
+  val impersonableString = new ImpersonableString("ALex")
+  impersonableString.greet()
+
+  // implicit way -> extension methods
+  "Peter".greet() // the compiler does:  new ImpersonableString("Peter").greet()
+
+  // implicit arguments and values
+  def increment(x: Int)(implicit y: Int) = x + y
+
+  implicit val b = 10
+
+  def increment2 = increment(20) // the compiler automatically completes with (10)
+
+
+  // more complex example
+  trait JSONSerializer[T] {
+    def toJSON(value: T): String
+  }
+
+  implicit val PersonJSONSerializer: JSONSerializer[Person] = (person: Person) => {
+    s"{ name: ${person.name}}"
+  }
+
+  def listToJson[T](values: List[T])(implicit serializer: JSONSerializer[T]): String =
+    values.map(serializer.toJSON).mkString("[", ",", "]")
+
+  // implicit method
+  implicit def oneArgCaseClassSerializer[T <: Product]: JSONSerializer[T] = (value: T) => {
+    s"{ ${value.productElementName(0)} : ${value.productElement(0)} }"
+  }
+
+  case class Cat(name: String)
+
+  // !! the compiler will AUTOMATICALLY create a JSON serializer of Cat and make it available
+
+  def main(args: Array[String]): Unit = {
+
+    println(oneArgCaseClassSerializer[Cat].toJSON(Cat("Arra")))
+    println(oneArgCaseClassSerializer[Person].toJSON(Person("Alex")))
+    println(listToJson(List(Cat("Arra"), Cat("Hatz John"))))
+  }
+}
